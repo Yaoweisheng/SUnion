2f87feeec1dfd47a1c108fce8f0483a3e6113ca0
Yaoweisheng <1027407651@qq.com>
2017-04-25 20:08:01 +0800

    teacherHistoryMessage.js

--- js/teacherHistoryMessage.js
+++ js/teacherHistoryMessage.js
@@ -3,7 +3,7 @@ var TeacherHistoryMessage = React.createClass({
     render: function(){
       return(
         <div>
-          <Header />
+      {/*    <Header />*/}
           <div className="h40"></div>
           <Time />
           <Students /> 
@@ -12,15 +12,15 @@ var TeacherHistoryMessage = React.createClass({
      }
     });
 
-var Header = React.createClass({
+/*var Header = React.createClass({
 	render : function(){
 		return (
   	       		<header>
- 		         <h3>发送消息</h3>
+ 		         <h3>老师历史消息</h3>
 	         	</header>			
 			);
 	}
-});
+});*/
 
 var Time = React.createClass({
 	render : function(){
@@ -71,11 +71,10 @@ var Student = React.createClass({
 	render :function(){
 		return (
 			<div className="student">
-				<div className="student_img"></div>
-				<div className="student_name">接收同学：×××</div>
-				<div className="student_number">学号：2015333123456</div>
-				<div className="message">发送内容:发送信息发送信息发送信息发送信息发送信息发送信息发送信息发送信息</div>
-				<div className="line"></div>
+			<div className="student_name">2017年4月14日</div>
+			<div className="message">发送内容:发送信息发送信息发送信息发送信息发送信息发送信息发送信息发送信息</div>
+			<div className="student_detail">查看详情</div>
+			<div className="line"></div>
 			</div>
 			);
 	}

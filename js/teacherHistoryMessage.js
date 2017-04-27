c6410aaa73b9b40757007690b0140a7aa1a768dd
yaoweisheng <1027407651@qq.com>
2017-04-27 19:11:59 +0800

    老师历史消息功能实现

--- js/teacherHistoryMessage.js
+++ js/teacherHistoryMessage.js
@@ -3,10 +3,8 @@ var TeacherHistoryMessage = React.createClass({
     render: function(){
       return(
         <div>
-      {/*    <Header />*/}
-          <div className="h40"></div>
           <Time />
-          <Students /> 
+          <Messages /> 
         </div>
       );
      }
@@ -54,32 +52,119 @@ var Search = React.createClass({
 	}
 });
 
-var Students = React.createClass({
+var Messages = React.createClass({
+	getDefaultProps: function() {
+	    return {
+	    	messages: [{"time":"2017-04-11 16:13:22","content":"今天不上课啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦，自由复习！！","students":[{"id":1,"sname":"曹辉","snumber":"1","headimageUrl":"img/student_img.png"},{"id":2,"sname":"璐璐","snumber":"2","headimageUrl":"img/student_img.png"}]},{"time":"2017-05-12 08:13:22","content":"嗯，今天很生气","students":[{"id":2,"sname":"璐璐","snumber":"2","headimageUrl":"img/student_img.png"}]}]
+	    	// message: ch.history.teacher
+	    };
+	},
+	getInitialState: function() {
+	    return {
+
+	    };
+	},
 	render :function(){
 		return (
 			<div className="students">
-				<Student />
-				<Student />
-				<Student />
-				<Student />
-				<Student />
+			{
+				this.props.messages.map(function(mess, index) {
+					return <Message key={index} message={mess} index={index} />
+				})
+			}
 			</div>
-			);
+		);
 	}
 });
-var Student = React.createClass({
+var Message = React.createClass({
+	getDefaultProps: function() {
+	    return {
+	    	
+	    };
+	},
+	getInitialState: function() {
+	    return {
+	    	show: false
+	    };
+	},
+	showChange: function(index){
+		this.setState({
+			show: !this.state.show
+		}, function(){
+			var s = {
+				show: this.state.show,
+				index: index
+			}
+			PubSub.publish("show", s)
+		})
+	},
 	render :function(){
 		return (
-			<div className="student">
-			<div className="student_name">2017年4月14日</div>
-			<div className="message">发送内容:发送信息发送信息发送信息发送信息发送信息发送信息发送信息发送信息</div>
-			<div className="student_detail">查看详情</div>
-			<div className="line"></div>
+			<div className={this.state.show?"student show":"student"}>
+				<div className="stu" onClick={this.showChange.bind(this, this.props.index)}>
+					<div className="student_name">{this.props.message.time}</div>
+					<div className="message">发送内容:{this.props.message.content}</div>
+					{
+						!this.state.show?<div className="student_detail">查看详情</div>:null
+					}
+				</div>
+				<Detail show={this.state.show} index={this.props.index} detail={this.props.message.students} />
+				<div className="line"></div>
 			</div>
-			);
+		);
 	}
 });
+var Detail = React.createClass({
+	getDefaultProps: function() {
+	    return {
+	    	
+	    };
+	},
+	getInitialState: function() {
+	    return {
+	    	height: "0px",
+	    	h2: this.props.detail.length * 45,
+	    };
+	},
+	componentDidMount: function () {
+		this.pubsub_token = PubSub.subscribe('show', function (topic, s) {
+			if(this.props.index == s.index) {
+				this.setState({height: s.show?(this.state.h2 + "px"):"0px"})
+			}
+		}.bind(this))
+	},
+	componentWillUnmount: function () {
+		PubSub.unsubscribe(this.pubsub_token)
+	},
+	render: function(){
+		
+		// if(this.props.show) {
+			return(
+				<div className="weui-cells" style={{height:this.state.height}}>
+				{
+					this.props.detail.map(function(stu, index) {
+						// alert(stu.id)
+							return (
+					            <div className="weui-cell" key={index}>
+					                <div className="weui-cell__bd">
+					                    <img src={stu.headimageUrl}/> <p className="name">{stu.sname}</p>
+					                </div>
+					                <div className="weui-cell__ft">{stu.snumber}</div>
+					            </div>
+					        )
+						}
+					
+					)
+				}
 
+				</div>
+			
+			)
+		// }
+		// else return null
+	}
+}
+)
 ReactDOM.render(
   <TeacherHistoryMessage />,
   document.getElementsByClassName("page2")[0]

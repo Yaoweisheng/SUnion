a694bcea90ef420701d419690603727dd55008a1
yaoweisheng <1027407651@qq.com>
2017-05-02 19:05:37 +0800

    ...

--- js/sendBarrage.js
+++ js/sendBarrage.js
@@ -1,29 +1,70 @@
 /*sendBarrage.js*/
+var func = function(i) {
+	// alert("func")
+	PubSub.publish('func', "这是个弹幕" + i)
+}
 var SendBarrage = React.createClass({
+	getDefaultProps: function() {
+	    return {
+	    };
+	},
+	getInitialState: function() {
+	    return {
+	    	index: 0
+	    };
+	},
+	componentDidMount: function () {
+		// this.refs.bottom.click()
+		func(this.state.index)
+	},
+	componentWillUnmount: function () {
+	},
+	click: function() {
+		this.setState({
+			index: this.state.index+1
+		}, function() {
+			func(this.state.index)
+		})
+	},
 	render: function(){
 		return (
 			<div >
+				<a ref="bottom" href="#bottom" className="toBottom" onClick={this.click} >bottom</a>
 				<Barrages />
-				<div className="fill"></div>
+				<div className="fill01"></div>
+				{/*<div className="fill02" id="bottom"></div>*/}
+				<a name="bottom"></a>
 				<Edit />
 			</div>);
 	}
 });
 
 var Barrages = React.createClass({
+	getInitialState: function() {
+	    return {
+	    	bas: []
+	    };
+	},
+    componentDidMount: function () {
+        this.pubsub_token = PubSub.subscribe('func', function (topic, data) {
+            // alert("get")
+            // alert(data)
+            var bas = this.state.bas
+            bas.push(data)
+            this.setState({bas: bas})
+        }.bind(this))
+    },
+    componentWillUnmount: function () {
+        PubSub.unsubscribe(this.pubsub_token)
+    },
 	render: function(){
 		return (
 			<div className="weui-cells">
-				<Barrage />
-				<Barrage />
-				<Barrage />
-				<Barrage />
-				<Barrage />
-				<Barrage />
-				<Barrage />
-				<Barrage />
-				<Barrage />
-				<Barrage />
+				{
+					this.state.bas.map(function(ba, index) {
+						return <Barrage key={index} ba={ba} />
+					})
+				}
 			</div>);
 	}
 });
@@ -32,8 +73,8 @@ var Barrage = React.createClass({
 	render: function(){
 		return (
 			<div className="weui-cell">
-				<UserName />
-				<Text />
+				{/*<UserName />*/}
+				<Text ba={this.props.ba} />
 				{/*<Illustration />*/}
 			</div>);
 	}
@@ -49,10 +90,13 @@ var UserName = React.createClass({
 });
 
 var Text = React.createClass({
+    componentDidMount: function () {
+    	this.refs.ba.style.backgroundColor = (rand(125, 255), rand(125, 255), rand(125, 255))
+    },
 	render: function(){
 		return (
 			<div className="weui-cell__bd text">
-				<p>弹幕弹幕幕弹幕弹幕弹幕弹幕弹幕弹幕弹幕弹幕</p>
+				<p ref="ba">{this.props.ba}</p>
 			</div>);
 	}
 });
